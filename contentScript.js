10ed69248e0ac8b0270c5f5feeb242f40cee2cf2
nahuelPanigo <48660617+nahuelPanigo@users.noreply.github.com>
2020-08-04 01:43:34 -0300

    changes
    see the results

--- contentScript.js
+++ contentScript.js
@@ -270,7 +270,7 @@ browser.runtime.onMessage.addListener( requests => {
 	if(requests.call==="peerRequests"){
 		console.log("naaaaaaaaaaaaaa");
 		peer++;
-		console.log("ahahahaaaa");
+		console.log(request);
 		pageManager.peerRequests(request.args,filesP,peer);
 		console.log("123465");
 	}
