10e58d94e4b4123263c2860fa60e0f0c17898b3c
nahuelPanigo <48660617+nahuelPanigo@users.noreply.github.com>
2020-08-07 19:34:19 -0300

    change in pop
    console log res

--- popup.js
+++ popup.js
@@ -20,7 +20,7 @@ browser.runtime.onMessage.addListener(function (message, sender,sendResponse) {
     console.log("jime");
     p.innerText="resultado 1";
     console.log("fea");
-    console.log(document.getElementsByClassName("resultados").children())
+    console.log(document.getElementById("res").children())
     document.appendChild(p);
     console.log("cara de culo");
     sendResponse({
